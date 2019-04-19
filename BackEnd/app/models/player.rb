02259d71e4f664a2f33c82c48eded5d35fe305e1
EliLauff <eli.lauffenburger@gmail.com>
2019-04-19 09:19:27 -0500

    backend working

--- BackEnd/app/models/player.rb
+++ BackEnd/app/models/player.rb
@@ -1,7 +1,6 @@
 class Player < ApplicationRecord
     has_many :matches_as_player1, :class_name => "Match", :foreign_key => "player1_id"
     has_many :matches_as_player2, :class_name => "Match", :foreign_key => "player2_id"
-    has_many :player1_as_player2, :through => :matches_as_player2, :source => :player2
-    has_many :player2_as_player1, :through => :matches_as_player1, :source => :player1
-
+    has_many :player1s_as_player2, :through => :matches_as_player2, :source => :player1
+    has_many :player2s_as_player1, :through => :matches_as_player1, :source => :player2
 end
